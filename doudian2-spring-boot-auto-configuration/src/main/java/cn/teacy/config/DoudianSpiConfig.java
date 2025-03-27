1b6bf0ecb482fc70b2bda5b765e6805d0f1be954
chrisis <chrisis58@foxmail.com>
2025-03-27 21:21:14 +0800

    feat: 添加默认的SPI认证拦截器
    - 重命名配置类名

---
+++ doudian2-spring-boot-auto-configuration/src/main/java/cn/teacy/config/DoudianSpiConfig.java
@@ -0,0 +1,41 @@
+package cn.teacy.config;
+
+import cn.teacy.common.property.DoudianProperties;
+import cn.teacy.doudian.interceptor.SpiAuthInterceptor;
+import cn.teacy.doudian.persistent.SpiAccessLogPersistent;
+import cn.teacy.doudian.register.SpiServiceRegistry;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@ConditionalOnClass(SpiServiceRegistry.class)
+@AutoConfiguration(
+        after = {DoudianDefaultConfig.class}
+)
+@EnableConfigurationProperties({DoudianProperties.class})
+public class DoudianSpiConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SpiServiceRegistry spiServiceRegistry(
+            DoudianProperties doudianProperties
+    ) {
+        return new SpiServiceRegistry(doudianProperties.getAdditionalPackages());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SpiAuthInterceptor spiAuthInterceptor(
+            DoudianProperties doudianProperties,
+            SpiAccessLogPersistent spiAccessLogPersistent
+    ) {
+        return new SpiAuthInterceptor(
+                doudianProperties.getAppKey(),
+                doudianProperties.getAppSecret(),
+                spiAccessLogPersistent
+        );
+    }
+
+}
