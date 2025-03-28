e2982548bc5cb16fdb21035f9f721b2da8806263
chrisis <chrisis58@foxmail.com>
2025-03-28 14:33:38 +0800

    refactor: 使用StreamUtils替代IoUtil以简化响应体读取逻辑

--- doudian2-spring-boot-core/src/main/java/cn/teacy/doudian/codec/DoudianApiDecoder.java
+++ doudian2-spring-boot-core/src/main/java/cn/teacy/doudian/codec/DoudianApiDecoder.java
@@ -1,19 +1,19 @@
 package cn.teacy.doudian.codec;
 
-import cn.hutool.core.io.IoUtil;
 import cn.teacy.common.constant.ApiResponseConstant;
 import cn.teacy.common.doudian.api.CommonResponse;
 import cn.teacy.common.exception.InvalidAccessTokenException;
 import cn.teacy.common.exception.InvalidRefreshTokenException;
 import cn.teacy.doudian.token.AccessTokenHolder;
 import cn.teacy.doudian.token.RefreshTokenHolder;
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.FeignException;
 import feign.Response;
 import feign.codec.Decoder;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StreamUtils;
 
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
@@ -40,14 +40,10 @@ public class DoudianApiDecoder implements Decoder {
     @SneakyThrows
     @Override
     public Object decode(Response response, Type type) throws FeignException {
-        String body = IoUtil.read(response.body().asInputStream(), StandardCharsets.UTF_8);
+        String body = StreamUtils.copyToString(response.body().asInputStream(), StandardCharsets.UTF_8);
 
-        CommonResponse<?> commonResponse = objectMapper.readValue(body, new TypeReference<>() {
-            @Override
-            public Type getType() {
-                return type;
-            }
-        });
+        JavaType javaType = objectMapper.getTypeFactory().constructType(type);
+        CommonResponse<?> commonResponse = objectMapper.readValue(body, javaType);
 
         if (!ApiResponseConstant.StatusCode.OK.equals(commonResponse.getCode())) {
             log.warn("API response error: {}", commonResponse);
