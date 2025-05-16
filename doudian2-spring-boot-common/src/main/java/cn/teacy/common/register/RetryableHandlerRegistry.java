7d146d1213df1311471750fe9582ddcb8fa12e7c
chrisis <chrisis58@foxmail.com>
2025-05-16 14:44:48 +0800

    feat: 为处理器注册中心添加缓存
    - 采用惰性计算，避免频繁计算集合输出
    - 修改后清除缓存

--- doudian2-spring-boot-common/src/main/java/cn/teacy/common/register/RetryableHandlerRegistry.java
+++ doudian2-spring-boot-common/src/main/java/cn/teacy/common/register/RetryableHandlerRegistry.java
@@ -10,18 +10,29 @@ public class RetryableHandlerRegistry {
 
     private final Set<RetryableResponseHandler> handlers = new HashSet<>();
 
+    private Set<RetryableResponseHandler> cachedHandlers = null;
+
     public RetryableHandlerRegistry addHandler(RetryableResponseHandler handler) {
         handlers.add(handler);
+        evictCache();
         return this;
     }
 
     public RetryableHandlerRegistry excludeHandler(RetryableResponseHandler handler) {
         handlers.remove(handler);
+        evictCache();
         return this;
     }
 
     public Set<RetryableResponseHandler> getHandlers() {
-        return handlers.stream().collect(Collectors.toUnmodifiableSet());
+        if (cachedHandlers == null) {
+            cachedHandlers = handlers.stream().collect(Collectors.toUnmodifiableSet());
+        }
+        return cachedHandlers;
+    }
+
+    private void evictCache() {
+        cachedHandlers = null;
     }
 
 }
